00facdc0bb98137b5cc8512689f9fedfd95a3e58
heesang <dlagmltkd12@naver.com>
2021-09-02 02:26:40 +0900

    querydsl 버전문제 해결

--- src/main/java/startproject/starbooks/StarbooksApplication.java
+++ src/main/java/startproject/starbooks/StarbooksApplication.java
@@ -3,12 +3,6 @@ package startproject.starbooks;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import startproject.starbooks.domain.dto.BooksResponseDto;
-import startproject.starbooks.domain.dto.Item;
-import startproject.starbooks.service.BookApiClient;
-import startproject.starbooks.service.BookInsert;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -19,7 +13,4 @@ public class StarbooksApplication {
         SpringApplication.run(StarbooksApplication.class, args);
 
     }
-
-
-
 }
