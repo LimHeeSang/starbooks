bce56fd45b86bc3392c45e324e08ca60d2d2f791
heesang <dlagmltkd12@naver.com>
2021-09-04 04:34:28 +0900

    cors 해결

--- src/main/java/startproject/starbooks/controller/BookController.java
+++ src/main/java/startproject/starbooks/controller/BookController.java
@@ -3,10 +3,7 @@ package startproject.starbooks.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import startproject.starbooks.domain.Book;
 import startproject.starbooks.repository.BookRepository;
 
@@ -19,6 +16,7 @@ public class BookController {
 
     private final BookRepository bookRepository;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api/books")
     public Page<Book> getAllBooks(
             @RequestParam("page") int page, //요청 페이지
@@ -51,9 +49,11 @@ public class BookController {
         return books;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api/books/{book_id}")
     public Book getBookById(@PathVariable Long book_id) {
         Book book = bookRepository.findById(book_id).orElse(null);
         return book;
     }
+
 }
